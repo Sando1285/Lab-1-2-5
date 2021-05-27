ab99a67ceb9877d7ece007fbc22d58210d5d1364
Sando1285 <72953820+Sando1285@users.noreply.github.com>
2021-05-28 01:42:10 +0300

    Add files via upload

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab_1
+{
+
+    class Program
+    {
+        delegate int Operation(int firstValue, int secondValue,int thirdValue);
+        static void Main(string[] args)
+        {
+            Operation operation = delegate (int firstValue, int secondValue, int thirdValue)
+            { 
+                return (firstValue + secondValue +thirdValue)/3;
+            };
+            int d = operation(4, 5,6);
+            Console.WriteLine(d);       // 9
+            Console.Read();
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            string action;
+            Console.WriteLine("введите число 1");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("введите число 2");
+            secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("выберите операцию: '+' '-' '*' '/' ");
+            action = Console.ReadLine();
+            switch(action)
+            {
+                case "+":
+                    Console.WriteLine(firstValue+secondValue);
+                    break;
+
+                case "-":
+                    Console.WriteLine(firstValue - secondValue);
+                    break;
+
+                case "*":
+                    Console.WriteLine(firstValue * secondValue);
+                    break;
+
+                case "/":
+                    if (secondValue==0)
+                    {
+                        Console.WriteLine("Ошибка");
+                    }
+                    else
+                    {
+                        Console.WriteLine(firstValue / secondValue);
+                    }
+                   
+                    break;
+
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
